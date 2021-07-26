0835775494b49ea3ea3875b1d8842b04d072fa82
Varracosta <varracosta@mail.ru>
2021-07-26 18:39:54 +1200

    Cleared up things a little bit and added screen shaking

--- Kakapo Strikes Back/Assets/Scripts/Stone.cs
+++ Kakapo Strikes Back/Assets/Scripts/Stone.cs
@@ -6,13 +6,13 @@ public class Stone : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D stomper;
 
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         GameObject enemy = other.gameObject;
         if (stomper.IsTouchingLayers(LayerMask.GetMask("Killable enemy")))
         {
             FindObjectOfType<UIManager>().AddToScore(enemy.GetComponent<DamageDealer>().GetPointsPerKill());
+            Debug.Log("I detected an enemy!");
             Destroy(enemy);
         }
     }
