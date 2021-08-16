3d0393affb527641e17c823bc0eab87704cc96ca
Varracosta <varracosta@mail.ru>
2021-08-16 18:45:49 +1200

    Work started

---
+++ Kakapo Strikes Back/Assets/Input Actions/PlayerInputHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnAttackInput(InputAction.CallbackContext context)
+    {
+
+    }
+}
