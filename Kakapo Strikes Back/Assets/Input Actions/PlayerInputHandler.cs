f5c29ed822442a8e3a862e5207ea455f80eaa4d2
Varracosta <varracosta@mail.ru>
2021-08-18 13:27:21 +1200

    Fixed problem with New Input System

--- Kakapo Strikes Back/Assets/Input Actions/PlayerInputHandler.cs
+++ Kakapo Strikes Back/Assets/Input Actions/PlayerInputHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerInputHandler : MonoBehaviour
 {
     private Vector2 RawMovementInput;
@@ -10,13 +11,13 @@ public class PlayerInputHandler : MonoBehaviour
     public int NormInputY { get; private set; }
     public bool JumpInput { get; private set; }
     public bool AttackInput { get; private set; }
-    
+
     public void OnMoveInput(InputAction.CallbackContext context)
     {
         RawMovementInput = context.ReadValue<Vector2>();
 
         NormInputX = (int)(RawMovementInput * Vector2.right).normalized.x;
-        NormInputY = (int)(RawMovementInput * Vector2.up).normalized.y; 
+        NormInputY = (int)(RawMovementInput * Vector2.up).normalized.y;
     }
 
     public void OnJumpInput(InputAction.CallbackContext context)
