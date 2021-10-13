1364b92dbbefdf9b9ce7e31ea0bb2eb6f2d17d07
Varracosta <varracosta@mail.ru>
2021-10-14 10:30:23 +1300

    Bug fix
    Level 4 can be successfully unlocked now in Level List

--- Kakapo Strikes Back/Assets/Scripts/StartCutscene.cs
+++ Kakapo Strikes Back/Assets/Scripts/StartCutscene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartCutscene : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class StartCutscene : MonoBehaviour
         if (other.gameObject.CompareTag("Kakapo"))
         {
             StartCutScene();
+            PlayerPrefs.SetString("LevelPassed", SceneManager.GetActiveScene().name);
         }
     }
     public void StartCutScene()
