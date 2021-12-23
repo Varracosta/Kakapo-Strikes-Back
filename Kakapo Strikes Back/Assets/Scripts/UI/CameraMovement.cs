cd96b6a7c1fced751c3f89fe77429e1d892dfbe1
Varracosta <varracosta@mail.ru>
2021-12-24 10:44:11 +1300

    Minor Changes

--- Kakapo Strikes Back/Assets/Scripts/UI/CameraMovement.cs
+++ Kakapo Strikes Back/Assets/Scripts/UI/CameraMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private float cameraSpeed;
+
     void Update()
     {
         transform.position += new Vector3(cameraSpeed * Time.deltaTime, 0, 0);
     }
+
+    
 }
